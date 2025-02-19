8c6d2a161b7d8e6fc76c5d5eaf9aebbcf2e9ec2d
Alisa Valiullina <avaliull@f1r2s1.codam.nl>
2025-02-19 15:03:50 +0100

    started work on error catching and free conditions

--- src/pipex.c
+++ src/pipex.c
@@ -1,6 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                       ::::::::             */
+/*   pipex.c                                           :+:    :+:             */
+/*                                                    +:+                     */
+/*   By: avaliull <avaliull@student.codam.nl>        +#+                      */
+/*                                                  +#+                       */
+/*   Created: 2025/02/19 14:51:28 by avaliull     #+#    #+#                  */
+/*   Updated: 2025/02/19 15:02:30 by avaliull     ########   odam.nl          */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ::::::::             */
 /*   pipex.c                                           :+:    :+:             */ /*                                                    +:+                     */ /*   By: avaliull <avaliull@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2025/02/13 18:11:38 by avaliull     #+#    #+#                  */
@@ -21,97 +33,95 @@
 //clean_exit then frees and closes eveyrthing, outputs error msg from sterror,
 //and exit(1)s the program. clean_exit is also used when no error is encountered.
 
-int	check_files(const char *infile_name, const char *outfile_name)
+int	cmd2_process(char *const command_argv[], const int pipe_fd[2],
+				  const int fd_out, const char **path_arr)
 {
-	//this should be replaced with whatever error management is required
-	ft_printf("checking if exists file1: %d\n", access(infile_name, F_OK));
-	ft_printf("checking if can read file1: %d\n", access(infile_name, R_OK));
-	ft_printf("checking if exists file2: %d\n", access(outfile_name, F_OK));
-	ft_printf("checking if can write file2: %d\n", access(infile_name, W_OK));
+	if (close(pipe_fd[1]) < 0)
+		ft_printf("close err in cmd2\n");
+	if (dup2(pipe_fd[0], STDIN_FILENO) < 0)
+		ft_printf("stdin dup err in cmd2\n");
+	if (dup2(fd_out, STDOUT_FILENO) < 0)
+		ft_printf("stdout dup err in cmd2\n");
+	try_execve(path_arr, command_argv);
 	return (0);
 }
 
-int	*setup_pipe(void)
+int	cmd1_process(char *const command_argv[], const int pipe_fd[2],
+				  const int fd_in, const char **path_arr)
 {
-	int	*pipefd;
-
-	pipefd = (int *) malloc (sizeof (int) * 2);
-	if (!pipefd)
-		return (NULL);
-	if (pipe(pipefd) == -1) {
-		ft_printf("%s\n", strerror(errno));
-		free(pipefd);
-		return (NULL);
-	}
-	return (pipefd);
-}
-
-void	cmd2_process(char *const command_argv[], int *pipefd, int fd_out, const char **path_arr)
-{
-	close(pipefd[1]);
-	dup2(pipefd[0], STDIN_FILENO);
-	dup2(fd_out, STDOUT_FILENO);
+	if (close(pipe_fd[0]) < 0)
+		ft_printf("close err in cmd1\n");
+	if (dup2(fd_in, STDIN_FILENO) < 0)
+		ft_printf("stdin dup err in cmd1\n");
+	if (dup2(pipe_fd[1], STDOUT_FILENO) < 0)
+		ft_printf("stdout err in cmd1\n");
 	try_execve(path_arr, command_argv);
+	return (0);
 }
 
-void	cmd1_process(char *const command_argv[], int *pipefd, int fd_in, const char **path_arr)
-{
-	close(pipefd[0]);
-	dup2(fd_in, STDIN_FILENO);
-	dup2(pipefd[1], STDOUT_FILENO);
-	try_execve(path_arr, command_argv);
-}
-
-void	pipex(char *const cmd1[], char *const cmd2[],
-		   int *pipefd, const int io_fd[2])
-{
-	const char **path_arr = find_env_path();
-	int	fork_check;
-
-	fork_check = fork();
-	if (fork_check == -1)
-		ft_printf("fork error\n");
-	else if (fork_check == 0)
-		cmd1_process(cmd1, pipefd, io_fd[0], path_arr);
-	else
-	{
-		fork_check = fork();
-		if (fork_check == -1)
-			ft_printf("fork error\n");
-		else if (fork_check == 0)
-			cmd2_process(cmd2, pipefd, io_fd[1], path_arr);
-		wait(NULL);
-	}
-	int i = -1;
-	if (path_arr)
-	{
-		while (path_arr[++i])
-			free((void *) path_arr[i]);
-		free(path_arr);
-	}
-	return ;
-}
-
-void	clean_exit(char *const *cmd1, char *const *cmd2, int *pipe_fd, int const io_fd[2])
+int	free_pipex(char *const *cmd1, char *const *cmd2, const char **path_arr,
+			   int err_check)
 {
 	int	i;
 
-	i = -1;
 	if (cmd1)
 	{
+		i = -1;
 		while (cmd1[++i])
 			free(cmd1[i]);
 		free((void *) cmd1);
 	}
-	i = -1;
 	if (cmd2)
 	{
+		i = -1;
 		while (cmd2[++i])
 			free(cmd2[i]);
 		free((void *) cmd2);
 	}
-	close(io_fd[0]);
-	close(io_fd[1]);
+	if (path_arr)
+	{
+		i = -1;
+		while (path_arr[++i])
+			free((void *) path_arr[i]);
+		free(path_arr);
+	}
+	return (err_check);
+}
+
+int	pipex(char *const cmd_argv[2], const int pipe_fd[2], const int io_fd[2])
+{
+	const char **path_arr = find_env_path();
+	char *const	*cmd1 = ft_split(cmd_argv[0], ' ');
+	char *const	*cmd2 = ft_split(cmd_argv[1], ' ');
+	int	fork_check;
+	int	perror_check;
+
+	if (!cmd1 || !cmd2 || !path_arr)
+		return (free_pipex(cmd1, cmd2, path_arr, -1));
+	fork_check = fork();
+	perror_check = 0;
+	if (fork_check == -1)
+		return (free_pipex(cmd1, cmd2, path_arr, -1));
+	else if (fork_check == 0)
+		perror_check = cmd1_process(cmd1, pipe_fd, io_fd[0], path_arr);
+	else
+	{
+		fork_check = fork();
+		if (fork_check == -1)
+			return (free_pipex(cmd1, cmd2, path_arr, -1));
+		else if (fork_check == 0)
+			perror_check = cmd2_process(cmd2, pipe_fd, io_fd[1], path_arr);
+		wait(NULL);
+	}
+	return (free_pipex(cmd1, cmd2, path_arr, perror_check));
+}
+
+void	clean_exit(int *pipe_fd, int const io_fd[2])
+{
+	if (io_fd[0] > 0)
+		close(io_fd[0]);
+	if (io_fd[1] > 0)
+		close(io_fd[1]);
 	if (pipe_fd)
 	{
 		close(pipe_fd[0]);
@@ -121,20 +131,39 @@ void	clean_exit(char *const *cmd1, char *const *cmd2, int *pipe_fd, int const io
 	exit(0);
 }
 
+int	*setup_pipe(void)
+{
+	int	*pipefd;
+
+	pipefd = (int *) malloc (sizeof (int) * 2);
+	if (!pipefd)
+		return (NULL);
+	if (pipe(pipefd) == -1)
+	{
+		perror(strerror(errno));
+		free(pipefd);
+		return (NULL);
+	}
+	return (pipefd);
+}
+
 int	main(int argc, char *argv[])
 {
-	char *const	*cmd1 = ft_split(argv[2], ' ');
-	char *const	*cmd2 = ft_split(argv[3], ' ');
 	// this shouldn't be constant probably cause we need to check each error
 	// or add a wrapper for open (seems hard and dumb, probably don't. just typecast as const)
-	int const	io_fd[2] = {open(argv[1], O_RDONLY),
-		open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777)};
-	int			*pipe_fd;
-	
+	int	io_fd[2];
+	int	*pipe_fd;
+
+	io_fd[0] = open(argv[1], O_RDONLY);
+	if (io_fd[0] < 0)
+		clean_exit(NULL, io_fd);
+	io_fd[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (io_fd[1] < 0)
+		clean_exit(NULL, io_fd);
 	if (io_fd[0] < 0 || io_fd[1] < 0)
 	{
 		ft_printf("error on open (replace)\n");
-		clean_exit(cmd1, cmd2, NULL, io_fd);
+		clean_exit(NULL, io_fd);
 	}
 	if (argc != 5)
 	{
@@ -142,12 +171,9 @@ int	main(int argc, char *argv[])
 		exit(1);
 	}
 
-	// we probablyt don't need this since open will check for erros
-	check_files(argv[1], argv[4]);
-
 	pipe_fd = setup_pipe();
 	if (!pipe_fd)
-		clean_exit(cmd1, cmd2, NULL, io_fd);
-	pipex(cmd1, cmd2, pipe_fd, io_fd);
-	clean_exit(cmd1, cmd2, pipe_fd, io_fd);
+		clean_exit(NULL, io_fd);
+	pipex((char *const[]){argv[2], argv[3]}, pipe_fd, io_fd);
+	clean_exit(pipe_fd, io_fd);
 }
